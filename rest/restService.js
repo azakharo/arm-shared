70fc718754c80a159fdc7483fb08a58f82888c1c
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2016-10-12 17:18:05 +0300

    acceptant2 test data were added for the tariffs and transactions.

--- rest/restService.js
+++ rest/restService.js
@@ -24,7 +24,7 @@ mod.service(
     let acceptant1Url = '';
     let acceptant2Url = '';
     //if (isRestDebug) {
-    //  var serverAddr = 'https://cp.sarov-itc.ru';
+    //  var serverAddr = 'https://mrd.sarov-itc.ru';
     //  baseURL = serverAddr + baseURL;
     //  turnoverBaseURL = serverAddr + turnoverBaseURL;
     //  personDataBaseURL = serverAddr + personDataBaseURL;
@@ -79,6 +79,10 @@ mod.service(
     // PUBLIC METHODS
 
     function getAccounts() {
+      if (isRestDebug) {
+        return restFake.getAccounts();
+      }
+
       var request = $http({
         method: "get",
         url: baseURL + "accounts"
@@ -2075,6 +2079,10 @@ mod.service(
 
     // Acceptant prices
     function getTariffs() {
+      if (isRestDebug) {
+        return restFake.getTariffs();
+      }
+
       let deffered = $q.defer();
 
       getTransportApp().then(
@@ -2235,6 +2243,10 @@ mod.service(
 
 
     function getPaymentsBy(dtStart, dtFinish, currencyIds, busNames) {
+      if (isRestDebug) {
+        return restFake.getPaymentsBy(dtStart, dtFinish, currencyIds, busNames);
+      }
+
       var deffered = $q.defer();
 
       var params = {
